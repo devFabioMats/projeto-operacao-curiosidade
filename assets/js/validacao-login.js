73c6fb0baa51e52bcfd474b91536470240d915c3
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-11-19 11:59:42 -0300

    validacao feita

--- assets/js/validacao-login.js
+++ assets/js/validacao-login.js
@@ -1,17 +1,38 @@
-const tentativas = 3;
+let tentativas = 3;
 const login = "fabio@gmail.com";
 const senha = 123;
+const btn = document.querySelector("#btn");
 
-function validacao(event) {
+btn.addEventListener("click", (event) => {
     event.preventDefault();
-    let login_usuario = document.getElementById("login").value;
-    let senha_usuario = document.getElementById("senha").value;
+    validacao();
+});
 
-    if (login_usuario == login && senha_usuario == senha) {
-        window.location.href = "./pages/tela-pagina-inicial.html";
-        return false;
-    } else {
-        tentativas--;
-        alert("Login ou senha inválido. Você tem " + tentativas + " tentativas;");
+function validacao() {
+    var login_usuario = document.getElementById("login").value;
+    var senha_usuario = document.getElementById("senha").value;
+
+    if (login_usuario === "" || senha_usuario === "") {
+        alert("⚠️ Todos os campos devem estar preenchidos.");
+        return;
     }
+
+    if (!login_usuario.includes("@") || !login_usuario.includes(".com")) {
+        alert("⚠️ O campo de login deve conter um '@' e '.com'.");
+        return;
+    }
+
+    if (login_usuario != login || senha_usuario != senha) {
+        tentativas--;
+        alert("❌ Login ou senha incorretos. Tente novamente. (Tentativas restantes: " + tentativas + ")");
+        if (tentativas == 0) {
+            document.getElementById("login").disabled = true;
+            document.getElementById("senha").disabled = true;
+        }
+        return;
+    } else {
+        alert("✅ Login realizado com sucesso!");
+        window.location.href = "../pages/tela-pagina-inicial.html";
+    }
+
 }
