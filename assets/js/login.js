f661b2fb1502b5757e8c5b068250d0ebcb80eed3
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-23 08:10:22 -0300

    tentnado mexer na logica do jwt no fetch

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,7 +4,7 @@ btn.addEventListener("click", event => {
     login();
 });
 
-function login() {
+async function login() {
     let email = document.getElementById('email').value;
     let senha = document.getElementById('senha').value;
 
@@ -23,41 +23,45 @@ function login() {
         senha
     };
 
-    console.log(JSON.stringify(loginUsuario));
-    const retornoLoginApi = fetch(`https://localhost:7123/oc-api/Usuario/Login`, {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(loginUsuario)
-        }).then(response => {
-            if (!response.ok) {
-                throw new Error('Falha na requisição');
-            }
-            //         // Armazene o token de autenticação no localStorage (se a API retornar um token)
-            //         if (data.token) {
-            //             localStorage.setItem('token', data.token);
-            //             localStorage.setItem('isAuthenticated', 'true');
-            //         }
-            localStorage.setItem('isAuthenticated', 'true');
-            window.location.href = "../pages/tela-pagina-inicial.html";
-        }).catch(error => {
-            console.error('Erro:', error);
-            window.alert('❌ Login ou senha incorretos. Tente novamente.');
-        });
+   
+
+    await fetch(`https://localhost:7123/oc-api/Usuario/Login`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(loginUsuario)
+    }).then(response => {
+        // if (!response.ok) {
+        //     throw new Error('Falha na requisição');
+        // }
+        const requisicao = response.json();
+        const token = requisicao.token;
+        localStorage.setItem('tokenUsuario', JSON.stringify(token));
+    }).catch(error => {
+        console.error('Erro:', error);
+        window.alert('❌ Login ou senha incorretos. Tente novamente.');
+    });
+
+    // console.log(token);
+    // if (token) {
+    //     localStorage.setItem('tokenUsuario', JSON.stringify(token));
+    //     //window.location.href = "../pages/tela-pagina-inicial.html";
+    //     localStorage.removeItem('tokenUsuario');
+    // }
 }
 
 function verificarAutenticacao() {
-    const isAuthenticated = localStorage.getItem('isAuthenticated');
-    if (isAuthenticated !== 'true') {
+    const isAuthenticated = localStorage.getItem(token);
+    if (isAuthenticated) {
         window.location.href = "../pages/tela-login.html";
     } else {
-        carregarColaboradores()
+        carregarColaboradores();
     }
 }
 
 function logout() {
-    localStorage.setItem('isAuthenticated', 'false');
+    localStorage.removeItem(isAuthenticated);
     window.location.href = "../pages/tela-login.html";
 }
