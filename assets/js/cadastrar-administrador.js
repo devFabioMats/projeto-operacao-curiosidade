2d8a5fa4712d9a2dfe082e9d783f7b2be5f577f6
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-29 16:40:51 -0300

    criado adminsitrador

---
+++ assets/js/cadastrar-administrador.js
@@ -0,0 +1,89 @@
+document.querySelector("#btn-gravar").addEventListener("click", (event) => {
+    event.preventDefault();
+    setTempoAtual();
+    verificarSessaoEGravar();
+});
+
+function validacao(nome, email, senha) {
+    if (nome == "") {
+        alert("⚠️ O campo nome deve estar preenchido.");
+        return false;
+    }
+
+    if (email == "") {
+        alert("⚠️ O campo email deve estar preenchido.");
+        return false;
+    } else if (!email.includes("@") || !email.includes(".")) {
+        alert("⚠️ O campo de login deve conter um '@' e o '.dominio'.");
+        return false;
+    }
+
+    if (senha == "") {
+        alert("⚠️ O campo senha deve estar preenchido.");
+        return false;
+    }
+
+    return true;
+}
+
+function gravar() {
+    let nome = document.getElementById("nome-cadastro").value;
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+
+    if (!validacao(nome, email, senha)) {
+        return;
+    }
+
+    let administrador = {
+        nome,
+        email,
+        senha
+    };
+
+    fetch(`https://localhost:7123/oc-api/Administrador`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(administrador)
+    }).then(response => {
+        if (!response.ok) {
+            console.log(response);
+            throw new Error('Falha na requisição');
+        }
+        return response.json();
+    }).then(data => {
+        window.location.href = "../pages/tela-administrador.html";
+    }).catch(error => console.error('Erro:', error));
+}
+
+function setTempoAtual() {
+    const d = new Date();
+    let horaAtual = d.getHours() * 60;
+    let minutoAtual = d.getMinutes();
+    let minutoTotal = horaAtual + minutoAtual;
+    localStorage.setItem('minutoTotal', minutoTotal);
+}
+
+function logout() {
+    localStorage.removeItem('tokenUsuario');
+    localStorage.removeItem('minutoTotal');
+    localStorage.removeItem('minutoLimite');
+    window.location.href = "../pages/tela-login.html";
+}
+
+function verificarSessaoEGravar() {
+    setTempoAtual();
+    if (localStorage.getItem('tokenUsuario') == 'false') {
+        window.location.href = "../pages/tela-login.html";
+        logout();
+        window.alert('⏰ Sessão expirada. Faça login novamente.');
+    } else if (Number.parseInt(localStorage.getItem('minutoLimite')) <= Number.parseInt(localStorage.getItem('minutoTotal'))) {
+        logout();
+        window.alert('⏰ Sessão expirada. Faça login novamente.');
+    } else {
+        gravar();
+    }
+}
